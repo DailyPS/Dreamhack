a85f2f2797c21f3d6f7823662ceb85252ea7c7e2
DailyLinux <baksa1849@gmail.com>
2022-04-22 21:43:33 +0900

    System Hack First.

---
+++ System/code/solve_me.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+  int sz = 0x30;
+  char *buf = (char *)malloc(sizeof(char) * sz);
+
+  puts("Hello World!");
+  printf("Education + Hack = ?\n");
+  fgets(buf, sz, stdin);
+
+  printf("%s\n", buf);
+  printf("%d\n", strncmp(buf, "DreamHack", 9));
+
+  if (!strncmp(buf, "DreamHack", 9))
+    printf("Welcome Hackers :)\\n");
+  else
+    printf("No No :/n");
+
+  return 0;
+}
